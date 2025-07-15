97f06827beed2ac055efe820c3c20b0b71e953d8
eXory2024 <exory@anio.software>
2025-07-15 23:16:34 +0200

    Add convertBytesToUnitInfo.

---
+++ project/export/convertBytesToUnitInfo.ts
@@ -0,0 +1,30 @@
+const KiB = 1024
+const MiB = KiB * 1024
+const GiB = MiB * 1024
+const TiB = GiB * 1024
+
+export function convertBytesToUnitInfo(size: number): [number, string, boolean] {
+	if (isNaN(size)) {
+		return [NaN, "N/A", false]
+	}
+
+	if (KiB > size) {
+		if (size === 1) {
+			return [1, "Byte", true]
+		}
+
+		return div(1, "Bytes")
+	} else if (MiB > size) {
+		return div(KiB, "KiB")
+	} else if (GiB > size) {
+		return div(MiB, "MiB")
+	} else if (TiB > size) {
+		return div(GiB, "GiB")
+	}
+
+	return div(TiB, "TiB")
+
+	function div(divisor: number, unit: string): [number, string, boolean] {
+		return [size / divisor, unit, size % divisor === 0]
+	}
+}
